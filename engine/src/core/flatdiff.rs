6f0fff1ba7b691c7e537da7fa25d17105948e7cf
Andrew Beck <andrew@yourloot.com.au>
2023-10-27 11:18:57 +1030

    tests

--- engine/src/core/flatdiff.rs
+++ engine/src/core/flatdiff.rs
@@ -111,3 +111,138 @@ impl<'a, T: FlatDiffSer> Serialize for FlatDiff<'a, T> {
         state.end()
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    #[warn(unused_imports)]
+    use super::*;
+    use ::serde::{Deserialize, Serialize};
+    use serde_json::json;
+
+    #[test]
+    fn test_simple() {
+        let s1 = InnerStruct::default();
+        let mut s2 = InnerStruct::default();
+
+        s2.f1 = 23;
+        expect(&s2, &s1, json!({
+            "f1": 23,
+        }));
+    }
+
+    #[test]
+    fn test_enum() {
+            // variant
+        let s1 = EnumStruct {
+            enum_: Enum::Var1,
+        };
+        let s2 = EnumStruct {
+            enum_: Enum::Var2 { f1: 23, f2: 42 },
+        };
+        expect(&s2, &s1, json!({
+            "f1": 23,
+            "f2": 42,
+            "enum_": "Var2",
+        }));
+
+        // field within current variant
+        let s3 = EnumStruct {
+            enum_: Enum::Var2 { f1: 23, f2: 45 },
+        };
+        expect(&s3, &s2, json!({
+            "f2": 45,
+        }));
+
+    }
+
+    #[test]
+    fn test_nested_atomic() {
+        #[derive(FlatDiffSer, Copy, Clone, PartialEq, Default)]
+        pub struct InnerStruct {
+            f1: u32,
+            atomic: AtomicStruct,
+        }
+        let s1 = InnerStruct::default();
+
+        let s2 = InnerStruct {
+            f1: 23,
+            atomic: AtomicStruct {
+                f1: 23,
+                f2: 42,
+            },
+        };
+        expect(&s2, &s1, json!({
+            "f1": 23,
+            "atomic": {
+                "f1": 23,
+                "f2": 42,
+            },
+        }));
+
+        // field within outer
+        let mut s2 = InnerStruct::default();
+        s2.f1 = 45;
+        expect(&s2, &s1, json!({
+            "f1": 45,
+        }));
+
+        // field within nested atomic
+        let mut s2 = InnerStruct::default();
+        s2.atomic.f2 = 45;
+        expect(&s2, &s1, json!({
+            "atomic": {
+                "f1": 0,
+                "f2": 45,
+            },
+        }));
+    }
+
+    #[derive(Serialize, Deserialize, Copy, Clone, PartialEq, Default)]
+    pub struct AtomicStruct {
+        f1: u32,
+        f2: u32,    
+    }
+
+    impl Atomic for AtomicStruct {}
+
+    #[derive(FlatDiffSer, Copy, Clone, PartialEq, Default)]
+    pub struct InnerStruct {
+        // #[delta(skip)]
+        f1: u32,
+        f2: u32,
+    }
+
+    // #[derive(FlatDiffSer, Copy, Clone, PartialEq)]
+    // pub struct OuterStruct {
+    //     // #[delta(skip)]
+    //     // location: Location,
+    //     enum_: Enum,
+    //     inner_struct: InnerStruct,
+    // }
+    
+    #[derive(FlatDiffSer, Copy, Clone, PartialEq)]
+    pub struct EnumStruct {
+        enum_: Enum,
+    }
+
+    #[derive(FlatDiffSer, Copy, Clone, PartialEq)]
+    enum Enum {
+        Var1,
+        Var2 {
+            f1: u32,
+            f2: u32,
+        },
+        Var3 {
+            inner_struct: InnerStruct,
+        },
+    }
+
+    fn expect<T: FlatDiffSer>(new_val: &T, old_val: &T, expected: serde_json::Value) {
+        let diff = FlatDiff(new_val, old_val);
+        let json = serde_json::to_string(&diff).unwrap();
+        let value: serde_json::Value = serde_json::from_str(&json).unwrap();
+        assert_eq!(value, expected);
+    }
+
+}
--- engine/src/race_tests.rs
+++ engine/src/race_tests.rs
@@ -1,6 +1,5 @@
 #[cfg(test)]
 mod tests {
-    use super::*;
     use crate::core::*;
     use crate::race::*;
 
@@ -32,11 +31,14 @@ mod tests {
         let event = Event {
             event: EventType::Activate,
         };
-        race.handle_event(event, &mut |_, _| Ok(()));
-        assert!(
-            matches!(race.state, State::Active { speed: 0.0 }),
-            "State was not Active as expected",
-        );
+        let result = race.handle_event(event, &mut |_, _| Ok(()));
+        assert_eq!(result, Ok(true));
+        if let State::Active { speed } = race.state {
+            assert_eq!(speed, 0.0);
+        } else {
+            panic!("State was not Active as expected");
+        }
+
     }
 
     #[test]
@@ -56,12 +58,12 @@ mod tests {
         };
         let result = race.handle_event(event, &mut |_, _| Ok(()));
         assert_eq!(result, Ok(true));
-        assert!(matches!(
-            race.line,
-            Line::Stbd {
-                location: Location { .. }
-            }
-        ));
+        if let Line::Stbd{location} = race.line {
+            assert_eq!(location.lat, loc1.0);
+            assert_eq!(location.lon, loc1.1);
+        } else {
+            panic!("Line was not Stbd as expected");
+        }
 
         // set a new location for stbd
         let result = race.update_location(Some(loc2), Some(speed));
@@ -69,7 +71,14 @@ mod tests {
         let event = Event {
             event: EventType::LineStbd,
         };
-        assert!(matches!(race.line, Line::Stbd { .. }));
+        let result = race.handle_event(event, &mut |_, _| Ok(()));
+        assert_eq!(result, Ok(true));
+        if let Line::Stbd{location} = race.line {
+            assert_eq!(location.lat, loc2.0);
+            assert_eq!(location.lon, loc2.1);
+        } else {
+            panic!("Line was not Stbd as expected");
+        }
 
         // set port and check that line is Both
         let result = race.update_location(Some(loc1), Some(speed));
@@ -80,14 +89,33 @@ mod tests {
         let result = race.handle_event(event, &mut |_, _| Ok(()));
         assert_eq!(result, Ok(true));
         assert!(matches!(race.line, Line::Both { .. }));
+        if let Line::Both{stbd, port, ..} = race.line {
+            assert_eq!(stbd.lat, loc2.0);
+            assert_eq!(stbd.lon, loc2.1);
+            assert_eq!(port.lat, loc1.0);
+            assert_eq!(port.lon, loc1.1);
+        } else {
+            panic!("Line was not Both as expected");
+        }
 
         // set a new location for port
-        let result = race.update_location(Some(loc2), Some(speed));
+        let loc3 = (42.3, -113.2);
+        let result = race.update_location(Some(loc3), Some(speed));
         assert_eq!(result, true);
         let event = Event {
             event: EventType::LinePort,
         };
-        assert!(matches!(race.line, Line::Both { .. }));
+        let result = race.handle_event(event, &mut |_, _| Ok(()));
+        assert_eq!(result, Ok(true));
+        if let Line::Both{stbd, port, ..} = race.line {
+            assert_eq!(stbd.lat, loc2.0);
+            assert_eq!(stbd.lon, loc2.1);
+            assert_eq!(port.lat, loc3.0);
+            assert_eq!(port.lon, loc3.1);
+        } else {
+            panic!("Line was not Both as expected");
+        }
+
     }
 
     #[test]
@@ -115,22 +143,19 @@ mod tests {
         bump(&mut race, 1000, 30, 31_000);
         race.start(&());
 
-        assert!(
-            matches!(
-                race.state,
-                State::Racing {
-                    start_time: 31_000,
-                    speed: 0.0,
-                    heading: 0.0
-                }
-            ),
-            "State was not Racing as expected",
-        );
+        if let State::Racing{start_time, speed, heading} = race.state {
+            assert_eq!(start_time, 31_000);
+            assert_eq!(speed, 0.0);
+            assert_eq!(heading, 0.0);
+        } else {
+            panic!("State was not Racing as expected");
+        }
 
         let event = Event {
             event: EventType::RaceFinish,
         };
-        race.handle_event(event, &mut |_, _| Ok(()));
+        let result = race.handle_event(event, &mut |_, _| Ok(()));
+        assert_eq!(result, Ok(true));
         assert!(
             matches!(race.state, State::Idle),
             "State was not Idle as expected",
